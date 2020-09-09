65102d8b69b384ddacf476394fc8b7a4554a4a16
matthewmjm <matthewmjm@yahoo.com>
2020-09-09 13:17:31 -0600

    update to trivia controller/mjm

--- backend/computer-mouse-trivia/app/controllers/trivia_controller.rb
+++ backend/computer-mouse-trivia/app/controllers/trivia_controller.rb
@@ -1,12 +1,14 @@
 require 'json'
 require 'rest-client'
 require 'pry'
+require 'pp'
 
 class TriviaController < ApplicationController
     def index
         response = RestClient.get 'https://opentdb.com/api.php?amount=5&category=18&type=multiple'
         data = JSON.parse(response)
         byebug
+        questions = data{}
 
 
     end
@@ -21,3 +23,40 @@ end
 #     incorrect_answers: trivia["incorrect_answers"]
 #   )
 # end
+
+
+# require 'rest-client'
+# require 'json'
+# require 'pry'
+
+# response = RestClient.get('https://rickandmortyapi.com/api/character')
+# data = JSON.parse(response)
+# characters = data["results"]
+# characters.map do |character|
+#     Character.create(name: character["name"], status: character["status"], gender: character["gender"])
+# end
+
+url = ''
+my_results = JSON.parse(RestClient.get(url))["results"]
+my_result = my_results[0]
+correct_answer = my_result["correct_answer"]
+incorrect_answers = my_result["incorrect_answers"]
+pp incorrect_answers
+incorrect_answers = incorrect_answers.map() do |answer|
+    {
+        text: answer,
+        is_correct: false
+    }
+end
+correct_answer = {
+    text: correct_answer,
+    is_correct: true
+}
+
+all_answers = incorrect_answers.concat([correct_answer]) 
+
+# to random array; but doesn't mutate
+# .shuffle
+# other methods that don't exist in JS
+# .sample()
+# (1..5).to_a
